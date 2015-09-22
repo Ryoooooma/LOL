ff2681594f473117b114943be3781d0f9551fd67
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-09-22 10:22:26 +0800

    Initial Commit

---
+++ LOL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOL
+//
+//  Created by 脇田竜馬 on 2015/09/22.
+//  Copyright (c) 2015年 Ryoma Wakita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
